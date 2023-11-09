f1ea2ca91e306f528d1cd5328124b2804000f1d9
ricardow3 <cadu@wurzmann.com>
2023-11-09 19:41:29 -0300

    faltando css apenas

--- melhorando_database.js
+++ melhorando_database.js
@@ -1,42 +1,42 @@
 
 //Remover Parenteses com numero de cada materia
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('questoes2 copy.db');
+// const sqlite3 = require('sqlite3');
+// const db = new sqlite3.Database('questoes2 copy.db');
 
-function atualizarSubmateria(id, submateria) {
-  return new Promise((resolve, reject) => {
-    db.run('UPDATE questoes SET submateria = ? WHERE id = ?', [submateria, id], (err) => {
-      if (err) {
-        return reject(err);
-      }
-      resolve();
-    });
-  });
-}
+// function atualizarSubmateria(id, submateria) {
+//   return new Promise((resolve, reject) => {
+//     db.run('UPDATE questoes SET submateria = ? WHERE id = ?', [submateria, id], (err) => {
+//       if (err) {
+//         return reject(err);
+//       }
+//       resolve();
+//     });
+//   });
+// }
 
-db.serialize(async () => {
-  try {
-    const rows = await new Promise((resolve, reject) => {
-      db.all('SELECT id, submateria FROM questoes', [], (err, rows) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(rows);
-      });
-    });
+// db.serialize(async () => {
+//   try {
+//     const rows = await new Promise((resolve, reject) => {
+//       db.all('SELECT id, submateria FROM questoes', [], (err, rows) => {
+//         if (err) {
+//           return reject(err);
+//         }
+//         resolve(rows);
+//       });
+//     });
 
-    for (const row of rows) {
-      // Remove apenas os números no final da string
-      const submateriaAtualizada = row.submateria.replace(/\s*\d+$/, '').trim();
-      await atualizarSubmateria(row.id, submateriaAtualizada);
-      console.log('Registro atualizado:', row.id, submateriaAtualizada);
-    }
-  } catch (err) {
-    console.error('Erro:', err.message);
-  } finally {
-    db.close();
-  }
-});
+//     for (const row of rows) {
+//       // Remove apenas os números no final da string
+//       const submateriaAtualizada = row.submateria.replace(/\s*\d+$/, '').trim();
+//       await atualizarSubmateria(row.id, submateriaAtualizada);
+//       console.log('Registro atualizado:', row.id, submateriaAtualizada);
+//     }
+//   } catch (err) {
+//     console.error('Erro:', err.message);
+//   } finally {
+//     db.close();
+//   }
+// });
 
 
 
@@ -77,3 +77,41 @@ db.serialize(async () => {
 //     db.close();
 //   }
 // });
+
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('questoes2 copy.db'); // Certifique-se de que o caminho do banco de dados está correto
+
+
+function atualizarNomeDaMateria(id, nomeDaMateria) {
+  return new Promise((resolve, reject) => {
+    db.run('UPDATE questoes SET materia = ? WHERE id = ?', [nomeDaMateria, id], (err) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve();
+    });
+  });
+}
+
+db.serialize(async () => {
+  try {
+    const rows = await new Promise((resolve, reject) => {
+      db.all('SELECT id, materia FROM questoes', [], (err, rows) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(rows);
+      });
+    });
+
+    for (const row of rows) {
+      const nomeAtualizado = row.materia.replace(/\n/g, ' ').trim(); // Substitui quebras de linha por espaço
+      await atualizarNomeDaMateria(row.id, nomeAtualizado);
+      console.log('Registro atualizado:', row.id, nomeAtualizado);
+    }
+  } catch (err) {
+    console.error('Erro:', err.message);
+  } finally {
+    db.close();
+  }
+});
