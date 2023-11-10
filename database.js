7cf5fa6754ab109d0d61d28b2aea2e709865d76e
ricardow3 <cadu@wurzmann.com>
2023-11-10 14:30:04 -0300

    ./

--- database.js
+++
@@ -1,9 +0,0 @@
-// database.js
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: '.\config\questoes.db',
-});
-
-module.exports = sequelize;
--- routes/exercicios.routes.js
+++
@@ -1,9 +0,0 @@
-// routes/exercicios.routes.js
-
-const express = require('express');
-const router = express.Router();
-const { getMaterias } = require('../controllers/exercicios.controller');
-
-router.get('/materias', getMaterias);
-
-module.exports = router;
